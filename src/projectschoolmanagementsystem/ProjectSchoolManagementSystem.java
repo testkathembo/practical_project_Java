28fd57fb79ee4adb12219cbb0d4e75421745d5c9
User_management <testkathembo@gmail.com>
2024-10-16 08:19:14 +0200

    commit by Dieudonne

---
+++ src/projectschoolmanagementsystem/ProjectSchoolManagementSystem.java
@@ -0,0 +1,13 @@
+package projectschoolmanagementsystem;
+
+
+public class ProjectSchoolManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
